e80bab3371e2c6dd2c742ef752d5a803a5f0f8d4
0x1m4o <chuntdevnx@gmail.com>
2023-04-16 17:29:09 +0700

    GetX Workers

--- lib/getx-kuldii/lesson-4/pages/homepage.dart
+++ lib/getx-kuldii/lesson-4/pages/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getx/controllers/count_controller.dart';
+import '../controllers/count_controller.dart';
 import 'textpage.dart';
 import 'package:get/get.dart';
 
